f1e38cf396a4c7927747f9cf97e645cb18e4f354
baisula001 <91481714+baisula001@users.noreply.github.com>
2022-05-07 20:43:31 +0200

    Update drills9.cpp

--- drills9.cpp
+++ drills9.cpp
@@ -21,36 +21,36 @@ void chapter942()
 {
     using namespace Chrono942;
     cout << "Chapter 9.4.2:\n";
-    Date today(1978, 6, 25);  // colloquial style
-    Date tomorrow = today;  // copy constructor
+    Date today(1978, 6, 25);                                            // colloquial style
+    Date tomorrow = today;                                              // copy constructor
     tomorrow.add_day(1);
     cout << "Today: " << today << endl;
     cout << "Tomorrow: " << tomorrow << endl;
-    Date invalid_date = Date(2004, 13, -5);   // verbose style
+    Date invalid_date = Date(2004, 13, -5);                             // verbose style
 }
 
 void chapter943()
 {
     using namespace Chrono943;
     cout << "Chapter 9.4.3:\n";
-    Date today(1978, 6, 25);  // colloquial style
-    Date tomorrow = today;  // copy constructor
+    Date today(1978, 6, 25);                                            // colloquial style
+    Date tomorrow = today;                                              // copy constructor
     tomorrow.add_day(1);
     cout << "Today: " << today << endl;
     cout << "Tomorrow: " << tomorrow << endl;
-    Date invalid_date = Date(2004, 13, -5);   // verbose style
+    Date invalid_date = Date(2004, 13, -5);                             // verbose style
 }
 
 void chapter971()
 {
     using namespace Chrono971;
     cout << "Chapter 9.7.1:\n";
-    Date today(1978, Date::jun, 25);  // colloquial style
-    Date tomorrow = today;  // copy constructor
+    Date today(1978, Date::jun, 25);                                    // colloquial style
+    Date tomorrow = today;                                              // copy constructor
     tomorrow.add_day(1);
     cout << "Today: " << today << endl;
     cout << "Tomorrow: " << tomorrow << endl;
-    Date invalid_date = Date(2004, Date::dec, -5);   // verbose style
+    Date invalid_date = Date(2004, Date::dec, -5);                      // verbose style
 }
 
 void chapter974()
@@ -59,15 +59,15 @@ void chapter974()
     cout << "Chapter 9.7.4:\n";
     Date default_d;
     cout << "Default date: " << default_d << endl;
-    Date today(1978, Date::jun, 25);  // colloquial style
-    Date tomorrow = today;  // copy constructor
+    Date today(1978, Date::jun, 25);                                    // colloquial style
+    Date tomorrow = today;                                              // copy constructor
     tomorrow.add_day(1);
     Date next_year = today;
     next_year.add_year(1);
     cout << "Today: " << today << endl;
     cout << "Tomorrow: " << tomorrow << endl;
     cout << "Next year: " << next_year << endl;
-    Date invalid_date = Date(2004, Date::dec, -5);   // verbose style
+    Date invalid_date = Date(2004, Date::dec, -5);                      // verbose style
 }
 
 int main()
